87afc360ef3167f8e3de2343cf440b37766cc4bd
Chua Shin Herh <chuashinherh@gmail.com>
2025-09-01 04:28:28 +0800

    Display notes in notes view

--- lib/services/crud/notes_service.dart
+++ lib/services/crud/notes_service.dart
@@ -38,11 +38,16 @@ class NotesService {
   List<DatabaseNote> _notes = [];
 
   static final NotesService _shared = NotesService._sharedInstance();
-  NotesService._sharedInstance();
+  NotesService._sharedInstance() {
+    _notesStreamController = StreamController<List<DatabaseNote>>.broadcast(
+      onListen: () {
+        _notesStreamController.sink.add(_notes);
+      },
+    );
+  }
   factory NotesService() => _shared;
 
-  final _notesStreamController =
-      StreamController<List<DatabaseNote>>.broadcast();
+  late final StreamController<List<DatabaseNote>> _notesStreamController;
 
   Stream<List<DatabaseNote>> get allNotes => _notesStreamController.stream;
 
@@ -319,7 +324,7 @@ class DatabaseNote {
 
   @override
   String toString() {
-    return "Note, ID: $id, userId = $userId, isSyncedWithCloud = $isSyncedWithCloud";
+    return "Note, ID: $id, userId = $userId, isSyncedWithCloud = $isSyncedWithCloud, Text = $text";
   }
 
   @override
