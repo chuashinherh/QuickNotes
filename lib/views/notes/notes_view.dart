b5849e22c36b85cd6de3d02a50f36c47033cf4e7
Chua Shin Herh <chuashinherh@gmail.com>
2025-09-01 22:58:25 +0800

    Migrated from local database to Firestore

--- lib/views/notes/notes_view.dart
+++ lib/views/notes/notes_view.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:quicknotes/constants/routes.dart';
 import 'package:quicknotes/enums/menu_action.dart';
 import 'package:quicknotes/services/auth/auth_service.dart';
-import 'package:quicknotes/services/crud/notes_service.dart';
+import 'package:quicknotes/services/cloud/cloud_note.dart';
+import 'package:quicknotes/services/cloud/firebase_cloud_storage.dart';
 import 'package:quicknotes/utilities/dialogs/logout_dialog.dart';
 import 'package:quicknotes/views/notes/notes_list_view.dart';
 
@@ -14,12 +15,12 @@ class NotesView extends StatefulWidget {
 }
 
 class _NotesViewState extends State<NotesView> {
-  late final NotesService _notesService;
-  String get userEmail => AuthService.firebase().currentUser!.email;
+  late final FirebaseCloudStorage _notesService;
+  String get userId => AuthService.firebase().currentUser!.id;
 
   @override
   void initState() {
-    _notesService = NotesService();
+    _notesService = FirebaseCloudStorage();
     super.initState();
   }
 
@@ -59,45 +60,30 @@ class _NotesViewState extends State<NotesView> {
           ),
         ],
       ),
-      body: FutureBuilder(
-        future: _notesService.getOrCreateUser(email: userEmail),
+      body: StreamBuilder(
+        stream: _notesService.allNotes(ownerUserId: userId),
         builder: (context, snapshot) {
           switch (snapshot.connectionState) {
-            case ConnectionState.done:
-              return StreamBuilder(
-                stream: _notesService.allNotes,
-                builder: (context, snapshot) {
-                  switch (snapshot.connectionState) {
-                    case ConnectionState.waiting:
-                    case ConnectionState.active:
-                      if (snapshot.hasData) {
-                        final allNotes = snapshot.data as List<DatabaseNote>;
-                        return NotesListView(
-                          notes: allNotes,
-                          onDeleteNote: (note) async {
-                            await _notesService.deleteNote(id: note.id);
-                          },
-                          onTap: (note) {
-                            Navigator.of(context).pushNamed(
-                              createOrUpdateNoteRoute,
-                              arguments: note,
-                            );
-                          },
-                        );
-                      } else {
-                        return Scaffold(
-                          body: Center(
-                            child: const CircularProgressIndicator(),
-                          ),
-                        );
-                      }
-                    default:
-                      return Scaffold(
-                        body: Center(child: const CircularProgressIndicator()),
-                      );
-                  }
-                },
-              );
+            case ConnectionState.waiting:
+            case ConnectionState.active:
+              if (snapshot.hasData) {
+                final allNotes = snapshot.data as Iterable<CloudNote>;
+                return NotesListView(
+                  notes: allNotes,
+                  onDeleteNote: (note) async {
+                    await _notesService.deleteNote(documentId: note.documentId);
+                  },
+                  onTap: (note) {
+                    Navigator.of(
+                      context,
+                    ).pushNamed(createOrUpdateNoteRoute, arguments: note);
+                  },
+                );
+              } else {
+                return Scaffold(
+                  body: Center(child: const CircularProgressIndicator()),
+                );
+              }
             default:
               return Scaffold(
                 body: Center(child: const CircularProgressIndicator()),
